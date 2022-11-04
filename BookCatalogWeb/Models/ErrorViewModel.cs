cdd4efb17a56f6ae902b84108f92dffa7714a18c
Violetta Bayeva <violetta.bayeva@dreamstormstudios.com>
2022-11-04 16:18:35 +0100

    Add project files.

---
+++ BookCatalogWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookCatalogWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
